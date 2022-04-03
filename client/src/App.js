55ce3ed6b4555d3cdb167604a1448188f188b65d
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-04-03 17:21:45 +0430

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+
+import Chat from "./components/Chat/Chat";
+import Join from "./components/Join/Join";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Chat from "./components/Chat";
-import Join from "./components/Join";
+
 const App = () => {
-  <Router>
-    <Route path="/" exact Component={Join} />
-    <Route path="/chat" Component={Chat} />
-  </Router>;
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
 };
 
 export default App;
